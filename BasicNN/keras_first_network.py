d00184569abce75094637098ecb7656ba175ac5c
secretID2 <secretidentity009@gmail.com>
2018-02-02 16:47:25 +0000

    Save and load weights

--- BasicNN/keras_first_network.py
+++ BasicNN/keras_first_network.py
@@ -7,7 +7,10 @@ Created on Fri Feb  2 10:52:55 2018
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.models import model_from_json
 import numpy
+import os
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load pima indians dataset
@@ -36,5 +39,43 @@ scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
-print("Input:",X[0,:],"->",Y[0])
-print("predict:",round(model.predict(X[0:1,:])))
+print("Input:",X[3,:],"->",Y[3])
+prediction=model.predict(X[3:4,:])
+print("predict:",round(prediction[0][0]))
+
+
+#___________Save And Load________________________________________________________
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+'''
+# later...
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
+# Fit the model
+loaded_model.fit(X, Y, epochs=150, batch_size=10)
+
+
+# evaluate the model
+scores = loaded_model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (loaded_model.metrics_names[1], scores[1]*100))
+'''
